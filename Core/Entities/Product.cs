b020fb0ac4fdf7647a695bc0829d6aa3cd075fb4
David Ciucă <ciucadavidstefan@gmail.com>
2024-01-12 17:06:22 +0200

    InitialCommit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,9 @@
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        // auto generated property 
+        public string Name { get; set; }
+    }
+}
